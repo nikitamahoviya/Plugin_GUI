c0e019127d44b4428a82f5e4ddaa254a1b5f9b0d
NIKITA MAHOVIYA <43717626+nikitamahoviya@users.noreply.github.com>
2021-07-30 23:20:42 +0530

    Create TextHelp.java

---
+++ VCell_info/TextHelp.java
@@ -0,0 +1,85 @@
+/*
+ * #%L
+ * Core commands for SciJava applications.
+ * %%
+ * Copyright (C) 2010 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.vcell.imagej.plugin;
+
+import org.scijava.ItemIO;
+import org.scijava.ItemVisibility;
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.command.Previewable;
+import org.scijava.plugin.Attr;
+import org.scijava.plugin.Menu;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import java.awt.Font;
+
+/**
+ * A test of text displays.
+ * 
+ * @author Curtis Rueden
+ */
+
+@Plugin(type = Command.class, menuPath = "Plugins>Sandbox>TextHelp",
+	headless = true, attrs = { @Attr(name = "no-legacy") })
+public class TextHelp implements Command, Previewable {
+
+	
+	@Parameter(visibility = ItemVisibility.MESSAGE)
+	private final String header = "<html><h2 style=\"font-family:'Times New Roman'\">Hello " + System.getProperty("user.name") + " !  Welcome to VCell Help</h2><html>";
+	
+	
+	  @Parameter(visibility = ItemVisibility.MESSAGE) 
+	  private final String header1= "<html><p style=\"font-family:'Optima'\">Virtual Cell is an advanced software platform for modeling  <br> "
+	  		+ "and simulating reaction kinetics,membrane transport <br>"
+	  		+ "and diffusion in the complex geometries</p><html>";
+	  
+	 
+
+	@Override
+	public void preview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
